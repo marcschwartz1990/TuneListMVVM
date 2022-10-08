83d76160e45c949fc3cdda334ac62438ae7d55da
Marc Schwartz <marcschwartz1990@gmail.com>
2022-10-08 00:32:40 -0400

    Initial Commit

---
+++ TuneListMVVM/TuneListMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneListMVVMApp.swift
+//  TuneListMVVM
+//
+//  Created by Marc-Developer on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TuneListMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
